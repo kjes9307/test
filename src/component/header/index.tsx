fa7c41a6ca4eae2e4089da68d96c43efa90fe905
kjes9307 <kjes9307@gmail.com>
2022-12-07 21:33:36 +0800

    fix input

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -2,16 +2,8 @@ import axios from 'axios'
 import {useUrlQueryParam} from 'utils/url'
 import { cleanObject } from 'utils';
 import { useDebounce  } from 'utils';
-
-export const useProject = (param:string) =>{
-    // 第一參數為 監控key值
-    return axios.get(`https://newsapi.org/v2/top-headlines?country=${param}&apiKey=dc01149c7c004404a03a135efd861e6e`)
-
-
-}
-
 export const Header = () =>{
-    const [, setParam] = useUrlQueryParam(["country"]);
-    // const {data} = useProject(useDebounce(param.newsSearch,1000))
-    return <input onChange={(e)=>setParam({country:e.target.value})}></input>
+    const [param, setParam] = useUrlQueryParam(["country"]);
+    const {country} = param
+    return <input value={country} onChange={(e)=>setParam({country:e.target.value})}></input>
 }
