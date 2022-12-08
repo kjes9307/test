0973ce98932c5f586296041e92169a8f271b5407
kjes9307 <kjes9307@gmail.com>
2022-12-08 19:30:52 +0800

    fix solution 2

--- src/component/solution2/index.tsx
+++ src/component/solution2/index.tsx
@@ -1,28 +1,42 @@
-import {useEffect,FC} from 'react'
+import {useState,useEffect,FC} from 'react'
 import {listSlice,getListData} from 'redux/slice/listSlice'
 import {useSelector,useAppDispatch} from 'redux/hook'
 import { Route, Routes } from "react-router";
-import { Link , Navigate } from 'react-router-dom';
+import { Link , Navigate , useNavigate , useLocation  } from 'react-router-dom';
 import Icon from 'component/Icon';
 import {IconName} from '@fortawesome/fontawesome-common-types';
 import {Container,Row,Col,Card } from 'react-bootstrap'
 import {Datatype} from 'utils/type'
+import {useDebounce} from 'utils'
 type DataProps =  {
   data : Datatype[]
 }
-type SideBarDataProps =  {
-    onChange: (val:string)=> void
-  }
+
 const Header = () =>{
-  const endStr = useSelector(state=>state.dataList.endStr)
   const dispatch = useAppDispatch() 
+  const [search,setSearch] = useState('')
+  const delayVal = useDebounce(search,1000)
+  const navigate = useNavigate();
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
-    dispatch(listSlice.actions.onChangeNav(e.currentTarget.value))
+    setSearch(e.currentTarget.value)
   }
-  return <input value={endStr} onChange = {handleChange}></input>
+  useEffect(()=>{
+    console.log("@")
+    if(delayVal !== '' ){
+      // let str = `http://localhost:3001/articles?country=${delayVal}`
+      navigate(`?country=${delayVal}`)
+      dispatch(listSlice.actions.onChangeNav(delayVal))
+      // dispatch(searchList(str))
+    }
+  },[delayVal,dispatch,navigate])
+  return <input value={search} onChange = {handleChange}></input>
 }
-const SideBar:FC<SideBarDataProps> = (props) =>{
-    const {onChange} = props
+const SideBar:FC = () =>{
+    const dispatch = useAppDispatch() 
+    const onHandleNews = (val:string)=>{
+      let value =val.split('=')[1]
+      dispatch(listSlice.actions.onChangeNav(value))
+    }
     let array:{name:string,icon:IconName,routes:string}[] = [
         {routes:"/articles?country=news",name:'熱門報導',icon:'envelope'},
         {routes:"/articles?country=tw",name:'台灣',icon:"envelope"},
@@ -37,7 +51,7 @@ const SideBar:FC<SideBarDataProps> = (props) =>{
                 
                 <li key={i.name} className='mt-1 d-flex align-items-center justify-content-center'>
                     <div><Icon icon={i.icon} color='black' size='1x' /></div>
-                    <Link to={`${i.routes}`} onClick={()=>{onChange(i.routes);}}>{i.name}</Link>
+                    <Link to={`${i.routes}`} onClick={()=>{onHandleNews(i.routes);}}>{i.name}</Link>
                 </li>
                 
             )})}
@@ -77,13 +91,10 @@ const MainPage:FC<DataProps> =(props) =>{
 export const Sol2 = () =>{
     const loading = useSelector(state=>state.dataList.loading)
     const list = useSelector(state=>state.dataList.list)
-    const endStr = useSelector(state=>state.dataList.endStr)
+    // const endStr = useSelector(state=>state.dataList.endStr)
     const dispatch = useAppDispatch()   
-    const onHandleNews = (val:string)=>{
-      let value =val.split('=')[1]
-      dispatch(listSlice.actions.onChangeNav(value))
-    }
-  
+    const location = useLocation()
+    const {search} = location
     useEffect(()=>{
         console.log("@")
         // if(params&&params.country !==''){
@@ -92,9 +103,11 @@ export const Sol2 = () =>{
         //   dispatch(getListData(str))
         //   return
         // }
-        let str= !endStr?`http://localhost:3001/articles`:`http://localhost:3001/articles?country=${endStr}`
+        let value = search.split('=')[1]
+        let str= !search ?`http://localhost:3001/articles`:`http://localhost:3001/articles?country=${value}`
+        // console.log(value,str,!search)
         dispatch(getListData(str))
-    },[endStr,dispatch])
+    },[dispatch,search])
     if(loading){
         return <h1>....loading</h1>
     }
@@ -103,7 +116,7 @@ export const Sol2 = () =>{
           <Header />
         <Row>
           <Col md='2'>
-          <SideBar onChange={onHandleNews}/>
+          <SideBar />
           </Col>
           <Col md='10'> 
           <Routes>
